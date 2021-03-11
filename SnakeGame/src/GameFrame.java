bef63098aa113b6d40e4829bffe634fb5a5ab73a
dragos220600 <77338808+dragos220600@users.noreply.github.com>
2021-03-11 15:38:55 +0200

    Add files via upload

---
+++ SnakeGame/src/GameFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    public GameFrame() {
+        add(new GamePanel());
+        setTitle("Snake Game");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
